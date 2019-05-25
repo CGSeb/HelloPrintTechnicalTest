213119b19c8eec62036cdc91fdf83074e48590d5
sebastien michon <sebastien.michon17@gmail.com>
2019-05-25 11:39:53 +0200

    Fixing docker compose and sql import

---
+++ docker/db_helloprint/sqls/helloprint.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+LOCK TABLES `users` WRITE;
+INSERT INTO `users` VALUES (1,'helloprint','xxx@xx.nl','P@ssw0rd!',1);
+UNLOCK TABLES;
